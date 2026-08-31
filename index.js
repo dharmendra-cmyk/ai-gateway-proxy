2152418f8d1764cac821d50d89be74c3a8af2df1
dharmendra-cmyk <dharmendra@ahluwaliastrategic.company>
2026-08-31 12:32:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { Pool } = require('pg');
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')('sk_test_51U5tMuH2Y5HUdNhvwxZq6rLZHSCu3sAK7ovvgwAxpcDp08ktSMgqKSgitrrk0WhnwSq7iELu66CZgMoMk93Y15u00os0IHPKT');
 
 const app = express();
 const port = process.env.PORT || 8080;
